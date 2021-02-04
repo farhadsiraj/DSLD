0526eaeb5742428641dd2b503151eb972b28b73e
Ricky Rhodes <ricky@rickyrhodes.com>
2021-02-03 19:17:41 -0500

    feat(FE): add svg to background

--- client/GlobalStyles.js
+++ client/GlobalStyles.js
@@ -1,12 +1,34 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
-  body {
+
+
+  html, body {
     margin: 0;
     padding: 0;
     font-family: Helvetica, sans-serif;
     font-size: 16px;
   }
+
+  .custom-shape-divider-top-1612397001 {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    overflow: hidden;
+    line-height: 0;
+}
+
+.custom-shape-divider-top-1612397001 svg {
+    position: relative;
+    display: block;
+    width: calc(130% + 1.3px);
+    height: 480px;
+}
+
+.custom-shape-divider-top-1612397001 .shape-fill {
+    fill: #F26627;
+}
 `
 
 export default GlobalStyle
--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -29,7 +29,7 @@ const Dropdown = styled.div`
   position: fixed;
   z-index: 9;
   left: 0;
-  top: 65px;
+  top: 7vh;
   background-color: white;
   width: 100%;
   height: 100%;
