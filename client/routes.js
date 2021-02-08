c296d19d1acfbc2271a8a95bf54543f75c61cb7f
Ricky Rhodes <ricky@rickyrhodes.com>
2021-02-08 06:06:19 -0500

    style(FE): export default FormCheck

--- client/routes.js
+++ client/routes.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 import { AuthProvider } from './components/contexts/AuthContext'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { FormCheck } from './components/FormCheck'
 import Home from './components/Home'
 import Signup from './components/Signup'
 import Dashboard from './components/Dashboard'
+import FormCheck from './components/FormCheck'
 import LoginForm from './components/LoginForm'
 import PrivateRoute from './components/PrivateRoute'
-import ForgotPassword from './components/ForgotPassword'
 import UpdateProfile from './components/UpdateProfile'
+import ForgotPassword from './components/ForgotPassword'
 
 class Routes extends Component {
   render() {
@@ -17,15 +17,15 @@ class Routes extends Component {
         <AuthProvider>
           <Switch>
             <Route exact path="/" component={Home} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/formcheck" component={FormCheck} />
+            <Route exact path="/login" component={LoginForm} />
+            <Route exact path="/forgot-password" component={ForgotPassword} />
             <PrivateRoute exact path="/dashboard" component={Dashboard} />
             <PrivateRoute
               exactpath="/update-profile"
               component={UpdateProfile}
             />
-            <Route exact path="/signup" component={Signup} />
-            <Route exact path="/formcheck" component={FormCheck} />
-            <Route exact path="/login" component={LoginForm} />
-            <Route exact path="/forgot-password" component={ForgotPassword} />
           </Switch>
         </AuthProvider>
       </Router>
